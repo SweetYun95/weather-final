35ea95a15ffc73c003e920b1ca65b3d65e94e32e
SweetYun95 <isr01154@naver.com>
2025-06-23 17:20:11 +0900

    2506231720

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -7,22 +7,21 @@ const weatherApi = axios.create({
    baseURL: BASE_URL,
    headers: {
       accept: 'application/json',
-  
    },
 })
 
-export const getTodayWeather = async () => {
+export const getTodayWeather = async ({cityName = 'incheon'}) => {
    const response = await weatherApi.get(`/weather`, {
       // https://api.openweathermap.org/data/2.5/weather?q=incheon&appid=AUTH_KEY&units=metric&lang=kr
       params: {
-         q,
+         q: cityName,
          appid: import.meta.env.VITE_WEATHER_API_KEY,
          units: 'metric',
          lang: 'kr',
       },
    })
 
-   return response
+   return response.data
 }
 
 export default weatherApi
--- src/features/weatherSlice.js
+++ src/features/weatherSlice.js
@@ -1,18 +1,35 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { getTodayWeather } from '../api/weatherApi'
 
-export const fetchTodays = createAsyncThunk('weather/fetchTodays', async () => {
-   const response = await getTodayWeather()
-   return response
+export const fetchTodays = createAsyncThunk('weather/fetchTodays', async ({ cityName }) => {
+   const data = await getTodayWeather({ cityName })
+   console.log(data)
+   return data
 })
 
 const weatherSlice = createSlice({
    name: 'weather',
    initialState: {
       today: null,
+      loading: false,
+      error: null,
    },
    reducers: {},
    extraReducers: (builder) => {
       builder
+         .addCase(fetchTodays.pending, (state) => {
+            state.loading = true
+            state.error = null
+         })
+         .addCase(fetchTodays.fulfilled, (state, action) => {
+            state.loading = false
+            state.today = action.payload
+         })
+         .addCase(fetchTodays.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.error.message
+         })
    },
 })
+
+export default weatherSlice.reducer
