c8fd55ed9af0482efa5879c712898db8590f1068
SweetYun95 <isr01154@naver.com>
2025-06-23 18:13:33 +0900

    2506231813

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,13 @@
+import { Wrap } from '../styles/styledComponents'
+
 function Nav() {
    return (
-      <ul>
-         <li>오늘의 날씨</li>
-         <li>주간 날씨</li>
-      </ul>
+      <Wrap>
+         <ul>
+            <li>오늘의 날씨</li>
+            <li>주간 날씨</li>
+         </ul>
+      </Wrap>
    )
 }
 
--- src/styles/styledComponents.js
+++ src/styles/styledComponents.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Wrap = styled.div`
    width: 100%;
+   height: 68px;
    overflow-x: hidden;
 `
 
