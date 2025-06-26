602f7e8467e97de4566bff1709364be8f6120dee
SweetYun95 <isr01154@naver.com>
2025-06-26 14:33:25 +0900

    2506261433

--- src/main.jsx
+++ src/main.jsx
@@ -10,10 +10,10 @@ import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
    // <StrictMode>
-      <Provider store={store}>
-         <BrowserRouter>
-            <App />
-         </BrowserRouter>
-      </Provider>
+   <Provider store={store}>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </Provider>
    // </StrictMode>
 )
